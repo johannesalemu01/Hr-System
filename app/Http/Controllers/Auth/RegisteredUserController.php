7ebee6581cf1e8434278b88c35c8468ba3a8ea7f
Yohannes Alemu <johannesalemu01@gmail.com>
2026-06-24 12:17:33 +0300

    Assign admin role to all new registrations

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        $user->assignRole('user');
+        $user->assignRole('admin');
 
 
 
